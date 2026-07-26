fa40cee5c4cb3a51920092a1e5cad96c3267b484
gueverstarstone <josephmungai028@gmail.com>
2026-07-26 19:50:37 +0300

    add a name validation and message pop up at the end of grade

--- project_3/report_card.py
+++ project_3/report_card.py
@@ -1,4 +1,4 @@
-student_name = input('Enter the name of the student: ').title()
+student_name = str(input('Enter the name of the student: ').strip().title())
 student_age = int(input('Enter student\'s age: '))
 
 english_score = int(input('Enter the score for English: '))
@@ -6,7 +6,11 @@ comm_skill_score = int(input('Enter the score for Communication Skills: '))
 net_score = int(input('Enter the score for Networking: '))
 math_score = int(input('Enter the score for Maths: '))
 
-if (english_score < 0 or english_score > 100 or
+if not student_name:
+    print('Name required....')
+elif not student_name.isalpha():
+    print("Please enter a vaid name.")
+elif (english_score < 0 or english_score > 100 or
     comm_skill_score < 0 or comm_skill_score > 100 or
     net_score < 0 or net_score > 100 or
     math_score < 0 or math_score > 100):
@@ -15,22 +19,28 @@ else:
     total_score = english_score + comm_skill_score + net_score + math_score
     average_score = total_score / 4
 
-    print(f'\nReport Card for {student_name} (Age: {student_age}')
+    print(f'\nReport Card for {student_name} (Age: {student_age})')
     print(f'English: {english_score}, Communication Skills: {comm_skill_score}, Networking: {net_score}, Maths: {math_score}')
     print(f'Total Score: {total_score}')
     print(f'Average Score: {round(average_score, 2)}')
 
-    if average_score >= 90:
+    if average_score >= 90 and student_age < 18:
         grade = ("A")
+        message = 'Young and Genius'
     elif average_score >= 80:
         grade = ("B")
-    elif average_score >= 70:
+        message = 'Excelent work, keep it up'
+    elif average_score >= 70 and average_score < 90:
         grade = ("C")
+        message = 'Good job, keep pushing for excelence'
     elif average_score >= 60:
         grade = ("D")
+        message = "Fair effort. There's room for improvement."
     elif average_score >= 50:
         grade = ("E")
+        message = "You passed, but keep practising."
     else:
         grade = ("F")
+        message = "Don't give up. Keep studying and you'll improve."
 
-    print(f'{student_name} scored a grade of {grade}')
+    print(f'{student_name} scored a grade of {grade}. {message}')
