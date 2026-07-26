f3678db138fa8793568bb0c905ec094c4d537f54
gueverstarstone <josephmungai028@gmail.com>
2026-07-26 19:12:12 +0300

    include grade on the final message

--- project_3/report_card.py
+++ project_3/report_card.py
@@ -1,13 +1,10 @@
-student_name = input('Enter the score for the name of the student: ').title()
+student_name = input('Enter the name of the student: ').title()
 student_age = int(input('Enter student\'s age: '))
 
 english_score = int(input('Enter the score for English: '))
 comm_skill_score = int(input('Enter the score for Communication Skills: '))
 net_score = int(input('Enter the score for Networking: '))
 math_score = int(input('Enter the score for Maths: '))
-
-if not student_name:
-    print('Name required....')
 
 if (english_score < 0 or english_score > 100 or
     comm_skill_score < 0 or comm_skill_score > 100 or
@@ -22,18 +19,18 @@ else:
     print(f'English: {english_score}, Communication Skills: {comm_skill_score}, Networking: {net_score}, Maths: {math_score}')
     print(f'Total Score: {total_score}')
     print(f'Average Score: {round(average_score, 2)}')
-    
-    if average_score >= 90:
-        print("A")
-    elif average_score >= 80:
-        print('B')
-    elif average_score >= 70:
-        print('C')
-    elif average_score >= 60:
-        print('D')
-    elif average_score >= 50:
-        print('E')
-    else:
-        print('F')
 
-print(f'{student_name} scored an average score of {average_score}: ')
+    if average_score >= 90:
+        grade = ("A")
+    elif average_score >= 80:
+        grade = ("B")
+    elif average_score >= 70:
+        grade = ("C")
+    elif average_score >= 60:
+        grade = ("D")
+    elif average_score >= 50:
+        grade = ("E")
+    else:
+        grade = ("F")
+
+    print(f'{student_name} scored a grade of {grade}')
