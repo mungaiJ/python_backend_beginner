525f26e8446a81f955dfbcea675c8e1d82232605
gueverstarstone <josephmungai028@gmail.com>
2026-08-11 22:06:12 +0300

    mini-project

--- scope_day_24/discount.py
+++ scope_day_24/discount.py
@@ -1,6 +1,5 @@
 def apply_discount(price,discount):
 
-    
     if not isinstance(price, (int , float)):
         return "The price should be a number"
     elif not isinstance(discount ,(int , float)):
