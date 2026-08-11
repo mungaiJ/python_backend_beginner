29874b11820d8c21e0cde13b8fdc50555fbbfd03
gueverstarstone <josephmungai028@gmail.com>
2026-08-11 22:04:16 +0300

    did a mini project for day 24

---
+++ scope_day_24/discount.py
@@ -0,0 +1,16 @@
+def apply_discount(price,discount):
+
+    
+    if not isinstance(price, (int , float)):
+        return "The price should be a number"
+    elif not isinstance(discount ,(int , float)):
+        return "The discount should be a number"
+    elif price <= 0:
+        return "The price should be greater than 0"
+    elif discount < 0  or discount > 100:
+        return "The discount should be between 0 and 100"
+
+    final_price = price - (price * discount / 100)
+    return final_price
+
+print(apply_discount(45.8,6.5))
---
+++ scope_day_24/scope.py
@@ -0,0 +1,22 @@
+def user_name():
+    name = 'Wantam'
+    return name
+
+result = user_name()
+# print(result)
+
+name = result
+
+def func_two(name):
+    return name
+
+print(f'my name is {name}')
+
+x = 10
+
+def test():
+    x = 20
+    return x
+    print(x)
+
+print(x)
